7d05b74199577e20d16718e6a77a31c886b00f6c
Vpk47 <145205038+Vpk47@users.noreply.github.com>
2023-10-14 13:47:22 +0530

    Inquiry changes css

--- stage/inquiryForm.php
+++ stage/inquiryForm.php
@@ -26,50 +26,37 @@
   <link href="css/responsive.css" rel="stylesheet" />
   <style>
       h2 {
-          text-align: center;
-      }
+                    text-align: center;
+                }
 
-      body {
-          font-family: Arial, sans-serif;
-      }
+                form {
+                    max-width: 400px;
+                    margin: 0 auto;
+                }
 
-      form {
-          max-width: 400px;
-          margin: 0 auto;
-          padding: 20px;
-          background-color: #f7f7f7;
-          border: 1px solid #ccc;
-          border-radius: 5px;
-      }
+                label {
+                    display: block;
+                    margin-bottom: 5px;
+                }
 
-      label {
-          display: block;
-          margin-bottom: 5px;
-          font-weight: bold;
-      }
+                input[type="text"],
+                input[type="email"],
+                textarea {
+                    width: 100%;
+                    padding: 10px;
+                    margin-bottom: 15px;
+                    border: 1px solid #ccc;
+                    border-radius: 3px;
+                }
 
-      input[type="text"],
-      input[type="email"],
-      textarea {
-          width: 100%;
-          padding: 10px;
-          margin-bottom: 15px;
-          border: 1px solid #ccc;
-          border-radius: 3px;
-      }
-
-      input[type="submit"] {
-          background-color: #007BFF;
-          color: #fff;
-          padding: 10px 20px;
-          border: none;
-          border-radius: 3px;
-          cursor: pointer;
-      }
-
-      input[type="submit"]:hover {
-          background-color: #0056b3;
-      }
+                input[type="submit"] {
+                    background-color: #007BFF;
+                    color: #fff;
+                    padding: 10px 20px;
+                    border: none;
+                    border-radius: 3px;
+                    cursor: pointer;
+                }
   </style>
 
 </head>
